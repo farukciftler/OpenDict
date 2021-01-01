bae42b58d67de7b39f705c96ef9954d1537c04df
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2021-01-01 07:54:11 +0300

    API Controllers Are Added

---
+++ OpenDict/Controllers/API/LocaleStringResourcesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace OpenDict.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocaleStringResourcesController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Merhaba";
+        }
+    }
+}
